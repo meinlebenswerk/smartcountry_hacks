0e5807ef0db973b3af29de0b29d3b0c904f581c2
paulkunze <mail@paulkunze.de>
2018-11-20 15:10:07 +0100

    Add frontend project

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# TrvVenueHotelClient
+
+## Running instructions
+
+After extracting the files from the zip folder, run `npm install` to install all required node modules. Then run `ng serve` for starting a dev server. Navigate to `http://localhost:4200/` to see the running application.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {LoginComponent} from "./views/login/login.component";
+import {RequestDetailsComponent} from "./views/request-details/request-details.component";
+import {NewRequestComponent} from "./views/new-request/new-request.component";
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'request-details',
+    component: RequestDetailsComponent
+  },
+  {
+    path: 'new-request',
+    component: NewRequestComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
