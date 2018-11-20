9d1baf00b1fb1acf8d83383d246919f48295831c
paulkunze <mail@paulkunze.de>
2018-11-20 19:55:05 +0100

    Refine login screen

--- frontend/src/app/views/login/login.component.ts
+++ frontend/src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) {
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    document.getElementById('loading-ring').style.display = 'inline-block';
+    setTimeout(() => {this.goToNextPage()}, 1000);
+  }
+
+  goToNextPage() {
+    this.router.navigate(['/home']);
   }
 
 }
